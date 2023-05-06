c55e749d0304c79959c2668d31dc4c019e232707
Thibault Lestang <t.lestang@imperial.ac.uk>
2023-05-06 11:36:29 +0100

    fix: tweaks to make it run

--- state.py
+++ state.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+import periods
+from infection import get_new_infections, get_load
+import parameters as p
+
 
 class Population:
 
@@ -21,18 +25,18 @@ class Population:
 
 
     def tick(self):
-        transition = np.logical_not(clock.astype(np.bool_))
-        new_s = self.diseased & ~self.infected & self.transition
-        new_d = self.infected & self.diseased & self.transition
-        new_id = self.infected & ~self.diseased & self.transition
-        new_i = get_new_infections(~self.infected, self.ages, self.bact_load, rng)
+        transition = np.logical_not(self.clock.astype(np.bool_))
+        new_s = self.diseased & ~self.infected & transition
+        new_d = self.infected & self.diseased & transition
+        new_id = self.infected & ~self.diseased & transition
+        new_i = get_new_infections(~self.infected, self.ages, self.bact_load, self.rng)
 
         self.clock[new_i] = periods.latent_time(self.latent_period_base[new_i])
         self.clock[new_id] = periods.id_time(
             self.ID_period_base[new_id], self.infection_counter[new_id]
         )
         self.clock[new_d] = periods.d_time(
-            self.D_period_base[new_d], self.infection_counter[new_d], ages[new_d]
+            self.D_period_base[new_d], self.infection_counter[new_d], self.ages[new_d]
         )
 
         self.diseased = self.diseased & ~new_s | new_d
@@ -40,20 +44,20 @@ class Population:
 
         # bacterial load
         self.bact_load[new_d] = 0
-        self.bact_load[new_i] = get_load(infection_counter[new_i])
+        self.bact_load[new_i] = get_load(self.infection_counter[new_i])
 
         # housekeeping
         self.infection_counter[new_i] += 1
         self.clock -= -1
-        self.age += 1
+        self.ages += 1
 
         # Death
-        dead = (rng.uniform(self.size) < p.bgrd_death_rate) | (self.age > p.MAX_AGE)
+        dead = (self.rng.uniform(0, 1, self.size) < p.bgrd_death_rate) | (self.ages > p.MAX_AGE)
         self.clock[dead] = 0
         self.diseased[dead] = 0
         self.infected[dead] = 0
         self.infection_counter[dead] = 0
-        self.age[dead] = 0
+        self.ages[dead] = 0
 
     def prevalence(age_bounds):
         i = self.age <= age_bounds[0] | self.age > age_bounds[1]
@@ -76,4 +80,4 @@ class Population:
 
         # Cast weights to integer to be able to count
         a, _ = np.histogram(self.age, bins=p.MAX_AGE, weights=i.astype(int))
-        return = a / self.size
+        return a / self.size
--- trachoma.py
+++ trachoma.py
@@ -1,51 +1,23 @@
 import numpy as np
 import init
 import parameters as p
-from infection import get_new_infections, get_load
-import periods
+from state import Population
 
 
 rng = np.random.default_rng()
-
 ages = init.ages(p.POP_SIZE, p.MAX_AGE, p.MEAN_AGE, rng)
 
-# Baseline disease periods
-latent_period_base = np.zeros(p.POP_SIZE) + 2
-ID_period_base=rng.poisson(lam=p.AV_ID_DURATION, size=p.POP_SIZE)
-D_period_base=rng.poisson(lam=p.AV_D_DURATION, size=p.POP_SIZE)
-
-newinf = init.infected(p.POP_SIZE, fraction=0.1, rng=rng)
-infected = infected | newinf
-infection_counter[newinf] = 1
-
-
-def compose(f, g):
-    return lambda x: f(g(x))
-
-def identity(f)
-
-from functools import reduce
-
-reduce(compose, [f]* n)
-
+pop = Population(ages)
 
 for t in range(0,10000):
-    transition = np.logical_not(clock.astype(np.bool_))
-    new_s = diseased & ~infected & transition
-    new_d = infected & diseased & transition
-    new_id = infected & ~diseased & transition
-    new_i = get_new_infections(~infected, ages, bact_load, rng)
+    pop.tick()
 
-    clock[new_i] = periods.latent_time(latent_period_base[new_i])
-    clock[new_id] = periods.id_time(ID_period_base[new_id], infection_counter[new_id])
-    clock[new_d] = periods.d_time(D_period_base[new_d], infection_counter[new_d], ages[new_d])
 
-    diseased = diseased & ~new_s | new_d
-    infected = infected & ~new_d | new_i
+    # def compose(f, g):
+#     return lambda x: f(g(x))
 
-    bact_load[new_d] = 0
-    bact_load[new_i] = get_load(infection_counter[new_i])
+# def identity(f)
 
-    # housekeeping
-    infection_counter[new_i] += 1
-    clock -= -1
+# from functools import reduce
+
+# reduce(compose, [f]* n)
