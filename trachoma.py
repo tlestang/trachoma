bcce906b5ee374453b1cec31b087d6ccf43faaba
Thibault Lestang <t.lestang@imperial.ac.uk>
2023-06-11 16:33:21 +0100

    Write some output to disk

--- trachoma.py
+++ trachoma.py
@@ -9,8 +9,21 @@ ages = init.ages(p.POP_SIZE, p.MAX_AGE, p.MEAN_AGE, rng)
 
 pop = Population(ages)
 
-for t in range(0,10000):
-    pop.tick()
+children = (1, 9)
+
+fchildren = open("data_children.csv", "w")
+f = open("data.csv", "w")
+
+for t in range(25 * 52):
+    data_children = f"{2*t},{pop.prevalence(children)},{pop.infection(children)}\n"
+    fchildren.write(data_children)
+    data_all = f"{2*t},{pop.prevalence((0, p.MAX_AGE))},{pop.infection((0, p.MAX_AGE))}\n"
+    f.write(data_all)
+    for tt in range(2):
+        pop.tick()
+
+fchildren.close()
+f.close()
 
 
     # def compose(f, g):
