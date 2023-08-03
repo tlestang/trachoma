dd881980791c7a97588cc2dd7a57586039ef8951
Thibault Lestang <thibault.lestang@protonmail.com>
2023-08-03 11:14:31 +0100

    implement background death

--- shift.c
+++ shift.c
@@ -25,6 +25,19 @@ void shift(uint8_t * A, int shift, int size) {
   }
 }
 
+void bgd_death(uint8_t *A, int idx, int size) {
+  uint8_t buf, mask;
+  int n, m, i;
+  m = idx % 8; // local index in byte block
+  n = (idx / 8) + 1; // size of subarray
+  for (mask = 1, i = 0; i < 8 - m - 1; ++i)
+    mask *= 2;
+  mask -= 1;
+  buf = A[n-1] & mask;
+  shift(A, 1, n);
+  A[n-1] = (A[n-1] & ~mask) | buf;
+}
+
 void rotate(uint8_t *A, int n, int size) {
   int i, k;
   k = n / 8;
@@ -41,7 +54,8 @@ int main() {
   int n = 5, i;
   uint8_t buf[2];
   printbytearray(b, n);
-  rotate(b, 17, n);
+  //rotate(b, 17, n);
+  bgd_death(b, 23, n);
   // shift(b, 3, 5);
   printbytearray(b, n);
 }
