e7d05792e5792948231c6c7a30ba9a62b29bf845
Thibault Lestang <thibault.lestang@protonmail.com>
2023-08-04 18:42:07 +0100

    Add header for shift utilities

---
+++ shift.h
@@ -0,0 +1,5 @@
+void printbytearray(uint8_t *b, int n);
+void bgd_death_bitarray(uint8_t*, int, int);
+void rotate_bitarray(uint8_t*, int, int);
+void rotate(int*, int, int, int);
+void rotate_double(double*, int, int, double);
