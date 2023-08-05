01c7897db4915c15036efcec939bc7b0b1d079ee
Thibault Lestang <thibault.lestang@protonmail.com>
2023-08-05 17:26:58 +0100

    Make Population class C-compatible

--- state.py
+++ state.py
@@ -1,95 +1,76 @@
+import ctypes
+from ctypes import POINTER, c_int, c_double, c_ubyte
 import numpy as np
 
 import periods
-from infection import get_new_infections, get_load
-import parameters as p
+from infection import get_load
 from init import infected
 
+
+class Pop_c(ctypes.Structure):
+    _fields_ = [
+        ("n", c_int),
+        ("inf", ctypes.POINTER(ctypes.c_ubyte)),
+        ("dis", ctypes.POINTER(ctypes.c_ubyte)),
+        ("lat", ctypes.POINTER(ctypes.c_ubyte)),
+        ("clockm", ctypes.POINTER(ctypes.c_int)),
+        ("ages", ctypes.POINTER(ctypes.c_int)),
+        ("count", ctypes.POINTER(ctypes.c_int)),
+        ("bactload", ctypes.POINTER(ctypes.c_double)),
+    ]
+
+
 class Population:
-
-    def __init__(self, ages):
-
-        self.rng = np.random.default_rng()
+    def __init__(self, ages, latent_base, rng):
 
         self.size = len(ages)
-        self.ages = ages
+        self.ages = ages.astype(np.int32)
+        self.clock = np.zeros(self.size, dtype=np.int32) - 1
+        self.count = np.zeros(self.size, dtype=np.int32)
+        self.bact_load = np.zeros(self.size, dtype=np.float64)
 
-        self.diseased = np.zeros(self.size, dtype=np.bool_)
-        self.clock = np.zeros(self.size) - 1
-        self.infection_counter = np.zeros(self.size)
-        self.bact_load = np.zeros(self.size)
+        self._latent = np.packbits(infected(self.size, 0.01, rng))
+        self._inf = self.latent.copy()
+        self._dis = np.packbits(np.zeros(self.size))
 
+        self.clock[self.latent] = periods.latent_time(latent_base[self.latent])
+        self.count[self.latent] = 1
+        ninfected = np.sum(self.count)
+        self.bact_load[self.latent] = get_load(np.zeros(ninfected) + 1)
 
-        # Baseline disease periods
-        self.latent_period_base = np.zeros(p.POP_SIZE) + 1
-        self.ID_period_base=self.rng.poisson(lam=p.AV_ID_DURATION, size=p.POP_SIZE)
-        self.D_period_base=self.rng.poisson(lam=p.AV_D_DURATION, size=p.POP_SIZE)
-
-        self.latent = infected(self.size, 0.01, self.rng)
-        self.infected = self.latent.copy()
-        self.clock[self.latent] = periods.latent_time(self.latent_period_base[self.latent])
-        self.bact_load[self.latent] = get_load(self.infection_counter[self.latent])
-        self.infection_counter[self.latent] = 1
-
-
-    def tick(self):
-        self.clock += -1
-        transition = np.logical_not(self.clock.astype(np.bool_))
-        new_s = self.diseased & ~self.infected & transition
-        new_d = self.infected & ~self.latent & transition
-        clearinf = self.latent & transition
-        new_i = get_new_infections(~self.infected, self.ages, self.bact_load, self.rng)
-        idx = np.array(range(0, 1000))
-        import pdb; pdb.set_trace()
-
-        self.clock[new_d] = periods.d_time(
-            self.D_period_base[new_d], self.infection_counter[new_d], self.ages[new_d]
-        )
-        self.clock[clearinf] = periods.id_time(
-            self.ID_period_base[clearinf], self.infection_counter[clearinf]
-        )
-        self.clock[new_i] = periods.latent_time(self.latent_period_base[new_i])
-
-        self.diseased = self.diseased & ~new_s | clearinf
-        self.infected = self.infected & ~new_d | new_i
-        self.latent = self.latent & ~clearinf | new_i
-
-        # bacterial load
-        self.bact_load[new_d] = 0
-        self.bact_load[clearinf] = get_load(self.infection_counter[clearinf])
-
-        # housekeeping
-        self.infection_counter[new_i] += 1
-        self.ages += 1
-
-        # Death
-        dead = (np.random.uniform(0, 1, self.size) < p.bgrd_death_rate) | (self.ages > p.MAX_AGE)
-        self.clock[dead] = -1
-        self.diseased[dead] = False
-        self.infected[dead] = False
-        self.latent[dead] = False
-        self.infection_counter[dead] = 0
-        self.ages[dead] = 0
-
-    def prevalence(self, age_bounds):
-        i = (self.ages >= age_bounds[0]) | (self.ages < age_bounds[1])
-        return np.count_nonzero(self.diseased[i]) / np.count_nonzero(i)
-
-    def infection(self, age_bounds):
-        i = (self.ages >= age_bounds[0]) | (self.ages < age_bounds[1])
-        return np.count_nonzero(self.infected[i]) / np.count_nonzero(i)
-
-    def group_size(self, age_bounds):
-        return np.count_nonzero(
-            (self.ages >= age_bounds[0]) | (self.ages < age_bounds[1])
+    @property
+    def _as_parameter_(self):
+        return Pop_c(
+            self.size,
+            self.inf.ctypes.data_as(POINTER(c_ubyte)),
+            self.dis.ctypes.data_as(POINTER(c_ubyte)),
+            self.latent.ctypes.data_as(POINTER(c_ubyte)),
+            self.clock.ctypes.data_as(POINTER(c_int)),
+            self.ages.ctypes.data_as(POINTER(c_int)),
+            self.count.ctypes.data_as(POINTER(c_int)),
+            self.bact_load.ctypes.data_as(POINTER(c_double)),
         )
 
-    def yearly_high_infection_count(threshold):
-        """Return propotion of individuals with no of infection
-        greater than threshold, per year group
-        """
-        i = self.infection_counter > threshold
+    @property
+    def inf(self):
+        return np.unpackbits(self._inf).astype(np.bool_)
 
-        # Cast weights to integer to be able to count
-        a, _ = np.histogram(self.age, bins=p.MAX_AGE, weights=i.astype(int))
-        return a / self.size
+    @inf.setter
+    def inf(self, a):
+        self._inf = np.packbits(a)
+
+    @property
+    def dis(self):
+        return np.unpackbits(self._dis).astype(np.bool_)
+
+    @dis.setter
+    def dis(self, a):
+        self._dis = np.packbits(a)
+
+    @property
+    def lat(self):
+        return np.unpackbits(self._lat).astype(np.bool_)
+
+    @lat.setter
+    def lat(self, a):
+        self._lat = np.packbits(a)
