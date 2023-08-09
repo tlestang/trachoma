945997fbab70cfc2bfc3e6bff7898ca3baf09b98
Thibault Lestang <thibault.lestang@protonmail.com>
2023-08-09 15:15:55 +0100

    Define main entry point func for processing scenario

--- process_scenario_definition.py
+++ process_scenario_definition.py
@@ -1,35 +1,40 @@
 from datetime import datetime, timedelta
-import json
 import sys
 
 import mda
 
-with open("scenario.json", "r") as f:
-    s = json.load(f)
 
-events = []
-for p in s["programs"]:
-    start = datetime.fromisoformat(s["start"])
-    end = datetime.fromisoformat(s["end"])
-    for inter in s["interventions"]:
-        t = inter.pop("type")
-        try:
-            cls = getattr(t, mda)
-        except AttributeError:
-            sys.stderr.write(
-                f"Error: could not find event type {t}.\n"
-            )
-            sys.exit(1)
-        d = inter.pop("interval")
-        interval = timedelta(weeks=d["weeks"])
-        event = cls(**inter)
-        events.append([
-            (event, start + i * interval)
-            for i in range(0, (start-end).days, interval.days)
-        ])
-events_flattened = []
-for e in events:
-    events_flattened.extend(e)
-events_flattened.sort(key=lambda x: x[1])
-            
-        
+def process_intervention(inter: dict, gen_dates):
+    t = inter.pop("type")
+    try:
+        cls = getattr(mda, t)
+    except AttributeError:
+        sys.stderr.write(
+            f"Error: could not find event type {t}.\n"
+        )
+        return []
+    interval = timedelta(weeks=inter.pop("interval"))
+    event = cls(**inter)
+    return [
+        (event, date) for date in gen_dates(interval)
+    ]
+
+
+def process_scenario_definition(d: dict):
+    all_events = []
+    for p in d["programs"]:
+        start = datetime.fromisoformat(p["start"])
+        end = datetime.fromisoformat(p["end"])
+
+        def gen_dates(interval):
+            date = start
+            while(date <= end):
+                yield date
+                date += interval
+
+        for events in map(
+                lambda x: process_intervention(x, gen_dates),
+                p["interventions"]
+        ):
+            all_events.extend(events)
+    return sorted(all_events, key=lambda x: x[1])
