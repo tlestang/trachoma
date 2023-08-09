6938ff6d31fe803b9380319ea3edf2c2475eb546
Thibault Lestang <thibault.lestang@protonmail.com>
2023-08-09 17:11:05 +0100

    Make mda call method register infection count

--- mda.py
+++ mda.py
@@ -2,33 +2,37 @@ import numpy as np
 
 
 class MDA:
-    def __init__(self, n, coverage, efficacy, rho):
-        self.n = n
-        self.treatment_count = np.zeros(n, dtype=np.int32)
+    def __init__(self, coverage, efficacy, rho):
         self.a = (1. / rho) - 1
         self.b = self.a * coverage
         self.rng = np.random.default_rng()
         self.count = 0
         self.efficacy = efficacy
 
-    def distribute(self):
+    def distribute(self, popsize):
         p = (
             self.b + self.treatment_count
         ) / (self.a + self.count)
         print(f"p = {p}")
-        return (self.rng.uniform(size=self.n) < p)
+        return (self.rng.uniform(size=popsize) < p)
 
     def __call__(self, pop):
-        t = self.distribute()
+        t = self.distribute(pop.size)
         ntreated = np.count_nonzero(t)
-        cured = np.zeros(self.n, dtype=np.bool_)
+        cured = np.zeros(pop.size, dtype=np.bool_)
         cured[t] = self.rng.uniform(size=ntreated) < self.efficacy
         inf = pop.inf  # Calls the 'inf' property on Population instance
         inf[cured] = False
         pop.inf = inf
         pop.bact_load[cured] = 0
 
+        attrname = f"mda_{id(self)}_treatment_count"
+        try:
+            treatment_count = getattr(pop, attrname)
+            treatment_count[t] += 1
+        except AttributeError:
+            setattr(pop, attrname, t.astype(np.int32))
+
         self.count += 1
-        self.treatment_count[t] += 1
 
         return ntreated, np.count_nonzero(cured)
