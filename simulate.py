4a0e72ab8ecc9e615db71a12f1a3ef7e2dd822fb
Thibault Lestang <t.lestang@imperial.ac.uk>
2023-08-10 17:10:34 +0100

    Pass beta value to simulate function

--- simulate.py
+++ simulate.py
@@ -11,11 +11,11 @@ lib.set_base_periods.argtypes = [
 ] * 3
 
 
-def simulate(p: Population, events: list):
+def simulate(p: Population, events: list, beta: float):
     for e, e_next in zip(events[:-1], events[1:]):
         nsteps = int((e_next[1] - e[1]).days / 7)
-        advance(p, nsteps)
-        e_next(p)
+        advance(p, nsteps, c_double(beta))
+        e_next[0](p)
 
 
 def set_base_periods(p, rng):
