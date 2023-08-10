9b591de80cc2634b392491f8cf0e2c6ff76f2ee3
Thibault Lestang <t.lestang@imperial.ac.uk>
2023-08-10 15:09:35 +0100

    Fix compilation errors in C implementation

--- trachoma.c
+++ trachoma.c
@@ -13,9 +13,6 @@ int *groups, ngroups;
 
 double BGD_DEATH_RATE;
 
-void get_infection_prob(int);
-double get_load(int);
-
 struct state {
   int n;
   uint8_t *inf;
@@ -27,14 +24,20 @@ struct state {
   double *bactload;
 };
 
-void apply_rules(struct state st, int times) {
+void get_infection_prob(int*, double*, int, double);
+double get_load(int);
+void update_indivs(struct state, uint8_t, uint8_t, uint8_t, int);
+int background_mortality(struct state, double, int);
+void old_age_mortality(struct state, int);
+
+void apply_rules(struct state st, int times, double beta) {
   int i, j, k, t;
 
   int nblocks = st.n / 8;
 
   for (t = 0; t < times; ++t) {
 
-    get_infection_prob(st.n);
+    get_infection_prob(st.ages, st.bactload, st.n, beta);
 
     // if first indiv in byte is at max_age, then all indivs after are
     // as well. No need to process these blocks.
@@ -59,8 +62,7 @@ void apply_rules(struct state st, int times) {
       st.inf[i] = st.inf[i] & ~new_d | new_i;
       st.lat[i] = st.lat[i] & ~clearinf | new_i;
 
-      update_indivs(new_i, clearinf, new_d,
-		    st.bactload, st.clockm, st.count);
+      update_indivs(st, new_i, clearinf, new_d, i);
     } // nblocks
 
     int n_old = background_mortality(st, BGD_DEATH_RATE, max_age);
@@ -70,6 +72,7 @@ void apply_rules(struct state st, int times) {
 
 int background_mortality(struct state st, double rate, int max_age) {
   int i;
+  int nblocks = st.n / 8;
   for (i = 0; st.ages[i] < max_age && i < st.n; ++i) {
     if (rand() / (double)RAND_MAX < rate) {
       bgd_death_bitarray(st.inf, i, nblocks);
@@ -94,12 +97,10 @@ void old_age_mortality(struct state st, int nold) {
   rotate_bitarray(st.inf, nold, nblocks);
   rotate_bitarray(st.dis, nold, nblocks);
   rotate_bitarray(st.lat, nold, nblocks);
-  }
 }
 
-void update_indivs(uint8_t new_i, uint8_t new_d,
-		   uint8_t clearinf, double bactload,
-		   int clockm, int count, int block_id) {
+void update_indivs(struct state st, uint8_t new_i, uint8_t new_d,
+		   uint8_t clearinf, int block_id) {
   int j, k;
   for (j=0; j < 8; ++j) {
     k = j + block_id * 8;
@@ -112,7 +113,7 @@ void update_indivs(uint8_t new_i, uint8_t new_d,
       continue;
     } else if ((clearinf << j) & '\x80') { // is new clearinf
       st.clockm[k] = setidtime(ID_base[k], st.count[k], st.ages[k]);
-      st.bactload[k] = get_load(count[k]);
+      st.bactload[k] = get_load(st.count[k]);
       continue;
     } else if ((new_i << j) & '\x80') { // is new I
       st.clockm[k] = setlatenttime(latent_base[k], st.count[k], st.ages[k]);
@@ -123,13 +124,12 @@ void update_indivs(uint8_t new_i, uint8_t new_d,
 
 
 
-#define BETA 0.21
-#define V_1 1.
-#define V_2 2.6
-#define PHI 1.4
-#define EPSILON 0.5
+double V_1;
+double V_2;
+double phi;
+double epsilon;
 
-void get_infection_prob(int n) {
+void get_infection_prob(int *ages, double* bactload, int n, double beta) {
   int n_prev, n_ingroup, igroup, k;
   double lam[3], pop_ratio[3], one_over_popsize, meanld, sum_ld;
   // unsigned int groups[] = {468, 780, 3121};
@@ -137,7 +137,7 @@ void get_infection_prob(int n) {
 
   k = 0;
   one_over_popsize = 1. / n;
-  epsm = 1. - EPSILON;
+  epsm = 1. - epsilon;
   
   for (k = 0, igroup = 0; igroup < ngroups; ++igroup) {
     n_prev = k;
@@ -146,7 +146,7 @@ void get_infection_prob(int n) {
 
     n_ingroup = (k - n_prev);
     meanld = sum_ld / n_ingroup;
-    lam[igroup] = BETA * V_1 * meanld + BETA * V_2 * pow(meanld, PHI + 1);
+    lam[igroup] = beta * V_1 * meanld + beta * V_2 * pow(meanld, phi + 1);
     pop_ratio[igroup] = (double)n_ingroup * one_over_popsize;
   }
 
@@ -168,85 +168,85 @@ double get_load(int ninf) {
   return b1 * exp((ninf - 1) * ep2);
 }
 
-int main() {
-  int n = 16;
-  int nblocks = n / 8;
-  prob = (double *) malloc(sizeof(double) * n);
+/* int main() { */
+/*   int n = 16; */
+/*   int nblocks = n / 8; */
+/*   prob = (double *) malloc(sizeof(double) * n); */
 
-  inf = (uint8_t *) malloc(sizeof(uint8_t) * nblocks);
-  dis = (uint8_t *) malloc(sizeof(uint8_t) * nblocks);
-  lat = (uint8_t *) malloc(sizeof(uint8_t) * nblocks);
-  clockm = (int *) malloc(sizeof(int) * n);
-  ages = (int *) malloc(sizeof(int) * n);
-  count = (int *) malloc(sizeof(int) * n);
-  bactload = (double *) malloc(sizeof(double) * n);
+/*   uint8_t *inf = (uint8_t *) malloc(sizeof(uint8_t) * nblocks); */
+/*   uint8_t *dis = (uint8_t *) malloc(sizeof(uint8_t) * nblocks); */
+/*   uint8_t *lat = (uint8_t *) malloc(sizeof(uint8_t) * nblocks); */
+/*   int *clockm = (int *) malloc(sizeof(int) * n); */
+/*   int *ages = (int *) malloc(sizeof(int) * n); */
+/*   int *count = (int *) malloc(sizeof(int) * n); */
+/*   double *bactload = (double *) malloc(sizeof(double) * n); */
 
-  ID_base = (int *) malloc(sizeof(int) * n);
-  D_base = (int *) malloc(sizeof(int) * n);
-  latent_base = (int *) malloc(sizeof(int) * n);
+/*   ID_base = (int *) malloc(sizeof(int) * n); */
+/*   D_base = (int *) malloc(sizeof(int) * n); */
+/*   latent_base = (int *) malloc(sizeof(int) * n); */
 
-  int ages_l[] = {203,  388,  586,  846, 1323,
-		  1327, 1400, 1446, 1824, 1917,
-		  2032, 2222, 2650, 3120, 3117,
-		  3119};
-  double bactload_l[] = {
-    0.54589385, 0.64449697, 0.94586293, 0.89743933, 0.74142636,
-    0.42041218, 0.30355981, 0.76096054, 0.62761568, 0.88280128,
-    0.16847746, 0.41943052, 0.56054159, 0.09329043, 0.67442931,
-    0.16027857
-  };
+/*   int ages_l[] = {203,  388,  586,  846, 1323, */
+/* 		  1327, 1400, 1446, 1824, 1917, */
+/* 		  2032, 2222, 2650, 3120, 3117, */
+/* 		  3119}; */
+/*   double bactload_l[] = { */
+/*     0.54589385, 0.64449697, 0.94586293, 0.89743933, 0.74142636, */
+/*     0.42041218, 0.30355981, 0.76096054, 0.62761568, 0.88280128, */
+/*     0.16847746, 0.41943052, 0.56054159, 0.09329043, 0.67442931, */
+/*     0.16027857 */
+/*   }; */
 
-  inf[0] =139; inf[1] = 239;
-  dis[0] = 62; dis[1] = 26;
-  lat[0] = 129; lat[1] = 229;
-  int i;
-  for (i = 0; i < n; ++i) {
-    count[i] = 0;
-    clockm[i] = 2;
-    ages[i] = ages_l[i];
-    bactload[i] = bactload_l[i];
-    ID_base[i] = 2;
-    latent_base[i] = 2;
-    D_base[i] = 2;
-  }
-  clockm[0] = 0; clockm[1] = -1;
-  clockm[5] = 0; clockm[10] = 0;
-  clockm[12] = 0;
+/*   inf[0] =139; inf[1] = 239; */
+/*   dis[0] = 62; dis[1] = 26; */
+/*   lat[0] = 129; lat[1] = 229; */
+/*   int i; */
+/*   for (i = 0; i < n; ++i) { */
+/*     count[i] = 0; */
+/*     clockm[i] = 2; */
+/*     ages[i] = ages_l[i]; */
+/*     bactload[i] = bactload_l[i]; */
+/*     ID_base[i] = 2; */
+/*     latent_base[i] = 2; */
+/*     D_base[i] = 2; */
+/*   } */
+/*   clockm[0] = 0; clockm[1] = -1; */
+/*   clockm[5] = 0; clockm[10] = 0; */
+/*   clockm[12] = 0; */
 
-  // for (i = 0; i < 8; ++i)
-    // printf("%c", clock[i] == 0 ? 'X' : 'O');
-  // printf(" ");
-  // for (i = 0; i < 8; ++i)
-    // printf("%c", clock[8 + i] == 0 ? 'X' : 'O');
-  // printf("\n");
-  /* printbytearray(dis, nblocks); */
-  /* printbytearray(inf, nblocks); */
-  /* printbytearray(lat, nblocks); */
+/*   // for (i = 0; i < 8; ++i) */
+/*     // printf("%c", clock[i] == 0 ? 'X' : 'O'); */
+/*   // printf(" "); */
+/*   // for (i = 0; i < 8; ++i) */
+/*     // printf("%c", clock[8 + i] == 0 ? 'X' : 'O'); */
+/*   // printf("\n"); */
+/*   /\* printbytearray(dis, nblocks); *\/ */
+/*   /\* printbytearray(inf, nblocks); *\/ */
+/*   /\* printbytearray(lat, nblocks); *\/ */
 
-  apply_rules(n, nblocks, 1);
-  // printf("\n");
+/*   apply_rules(n, nblocks, 1); */
+/*   // printf("\n"); */
 
-  // printf("%c", '\n');
+/*   // printf("%c", '\n'); */
 
-  /* printbytearray(dis, nblocks); */
-  /* printbytearray(inf, nblocks); */
-  /* printbytearray(lat, nblocks); */
-}
+/*   /\* printbytearray(dis, nblocks); *\/ */
+/*   /\* printbytearray(inf, nblocks); *\/ */
+/*   /\* printbytearray(lat, nblocks); *\/ */
+/* } */
 
-void set_arrays(uint8_t *inf_m, uint8_t *dis_m, uint8_t *lat_m,
-	 int *clock_m, int *ages_m, int *count_m,
-	 double *bactload_m, double *prob_m) {
-  ages = ages_m;
-  inf = inf_m;
-  dis = dis_m;
-  lat = lat_m;
-  clockm = clock_m;
-  count = count_m;
-  bactload = bactload_m;
-  prob = prob_m;
-}
+/* void set_arrays(uint8_t *inf_m, uint8_t *dis_m, uint8_t *lat_m, */
+/* 	 int *clock_m, int *ages_m, int *count_m, */
+/* 	 double *bactload_m, double *prob_m) { */
+/*   ages = ages_m; */
+/*   inf = inf_m; */
+/*   dis = dis_m; */
+/*   lat = lat_m; */
+/*   clockm = clock_m; */
+/*   count = count_m; */
+/*   bactload = bactload_m; */
+/*   prob = prob_m; */
+/* } */
 
-void set_base_periods(int *latent_base_m, *ID_base_m, int *D_base_m) {
+void set_base_periods(int *latent_base_m, int *ID_base_m, int *D_base_m) {
   D_base = D_base_m;
   ID_base = ID_base_m;
   latent_base = latent_base_m;
@@ -256,12 +256,12 @@ void set_infection_parameters(double v1_m, double v2_m,
 			      double phi_m, double eps_m) {
   V_1 = v1_m;
   V_2 = v2_m;
-  PHI = phi_m;
-  EPSILON = eps_m;
+  phi = phi_m;
+  epsilon = eps_m;
 }
 
 void set_background_mortality(double rate) {
-  BGD_DEATH_RATE = rate
+  BGD_DEATH_RATE = rate;
 }
 
 void set_groups(int groups_m[], int ngroups_m) {
