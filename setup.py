d9f4572864ae75fa29b179e87af4aea1e2f571d9
Thibault Lestang <t.lestang@imperial.ac.uk>
2023-08-21 16:37:09 +0100

    Add missing C files to list of sources in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import Extension, setup
 setup(
     ext_modules=[
         Extension(
-            name="ntd-trachoma.libtrachoma",
-            sources=["src/libtrachoma/trachoma.c"],
+            name="ntdmc_trachoma.libtrachoma",
+            sources=[
+                "src/libtrachoma/periods.c",
+                "src/libtrachoma/shift.c",
+                "src/libtrachoma/trachoma.c"
+            ],
         ),
     ]
 )
