28c1e0c3166c23d33b480d074d3998d48733a457
Thibault Lestang <thibault.lestang@meteo.fr>
2024-05-16 10:01:21 +0200

    test: Computation of treatment probability

---
+++ tests/test_mda.py
@@ -0,0 +1,47 @@
+from unittest.mock import patch
+import numpy as np
+from ntdmc_trachoma.state import Population
+from ntdmc_trachoma.mda import MDA, MDA_data
+
+rng = np.random.default_rng()
+ages = rng.integers(low=1, high=60, size=16)
+a, b = (0.2, 0.3)
+
+def test_draw_tment_prob():
+    pop = Population(ages)
+    current_ages = pop.ages[np.argsort(pop.indexes)]
+    data = MDA_data(
+        ages=current_ages,  # Sorted according to indexes
+        treatment_probability=rng.beta(a, b, size=16),
+        beta_dist_params=(a, b),
+    )
+    # Pretend that individuals 5, 9, 13, 14 and 15 were reset since
+    # the last MDA event.
+    pop.indexes = np.array(
+        [13, 14, 15, 5, 9, 0, 1, 2, 3, 4, 6, 7, 8, 10, 11, 12],
+        dtype=np.int32,
+    )
+    # Reorder array according to resets and age all individuals by
+    # arbitrary duration (e.g. 1 week)
+    pop.ages = pop.ages[pop.indexes] + 1
+    # Set age of reset individuals to something smaller than their
+    # previous ages before being reset, e.g. 0
+    pop.ages[0:5] = 0
+    with patch("ntdmc_trachoma.mda.np.random.default_rng") as mock:
+        expected_tment_probs = rng.beta(a, b, size=5)
+        mock.return_value.beta.return_value = expected_tment_probs
+        mda = MDA(1., 1., 1.)
+    mda.a, mda.b = (a, b)
+    # Remember that tment_prob is sorted according to increasing
+    # individual indexes and not according to ages.
+    tment_prob = mda.draw_tment_prob(
+        pop.ages[np.argsort(pop.indexes)], data
+    )
+    np.testing.assert_allclose(
+        tment_prob[[5, 9, 13, 14, 15]], expected_tment_probs,
+    )
+
+
+def test_draw_tment_prob_parameter_change():
+    #  TODO
+    pass
